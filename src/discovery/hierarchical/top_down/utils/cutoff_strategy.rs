a09bb5535fdcc11cf131661284d48ed7d2cf8992
peeva <peeva.viki@gmail.com>
2026-04-09 14:01:59 +0200

    implement the highest difference cutoff strategy

--- src/discovery/hierarchical/top_down/utils/cutoff_strategy.rs
+++ src/discovery/hierarchical/top_down/utils/cutoff_strategy.rs
@@ -6,6 +6,12 @@ pub struct HighestDifference;
 
 impl CutoffStrategy for HighestDifference {
     fn cutoff(&self, values: &[u32]) -> u32 {
-        todo!()
+        let mut sorted = values.to_vec();
+        sorted.sort_unstable_by(|a, b| b.cmp(a));
+
+        sorted.windows(2)
+            .max_by_key(|w| w[0] - w[1])
+            .map(|w| w[0])
+            .unwrap_or(sorted[0])
     }
 }
