fb4b8fa65cfe783b9c6400113e8de34732e8f7a2
peeva <peeva.viki@gmail.com>
2026-04-09 14:01:40 +0200

    update how dfg is filtered and add inverted filtering

--- src/discovery/utils/dfg.rs
+++ src/discovery/utils/dfg.rs
@@ -21,5 +21,42 @@ pub fn filter_dfg_absolute<'a>(dfg: &DirectlyFollowsGraph<'a>, threshold: u32) -
         result.add_end_activity(activity.clone());
     }
 
+    let to_remove: Vec<_> = result.activities.keys().filter(|&a| {
+        result.get_ingoing_df_relations(a).is_empty() &&
+            result.get_outgoing_df_relations(a).is_empty() }).cloned().collect();
+
+    to_remove.iter().for_each( |a| result.remove_activity(a));
+
+    result
+}
+
+pub fn inverted_filter_dfg_absolute<'a>(dfg: &DirectlyFollowsGraph<'a>, threshold: u32) ->
+                                                                                DirectlyFollowsGraph<'a> {
+    let mut result = DirectlyFollowsGraph::new();
+
+    for (activity, freq) in &dfg.activities {
+        result.add_activity(activity.clone(), *freq);
+    }
+
+    for ((from, to), freq) in &dfg.directly_follows_relations {
+        if *freq < threshold {
+            result.add_df_relation(from.clone(), to.clone(), *freq);
+        }
+    }
+
+    for activity in &dfg.start_activities {
+        result.add_start_activity(activity.clone());
+    }
+
+    for activity in &dfg.end_activities {
+        result.add_end_activity(activity.clone());
+    }
+
+    let to_remove: Vec<_> = result.activities.keys().filter(|&a| {
+        result.get_ingoing_df_relations(a).is_empty() &&
+            result.get_outgoing_df_relations(a).is_empty() }).cloned().collect();
+
+    to_remove.iter().for_each( |a| result.remove_activity(a));
+
     result
 }
