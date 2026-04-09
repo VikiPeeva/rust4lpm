fd93eaeb44f1a3387799de9faf05daf45fff6ca9
peeva <peeva.viki@gmail.com>
2026-04-09 14:02:23 +0200

    no need for artificial start and end in rendering the dfg

--- src/core/models/dfg/mod.rs
+++ src/core/models/dfg/mod.rs
@@ -8,23 +8,9 @@ impl<'a> DotRenderable for DirectlyFollowsGraph<'a> {
         writeln!(out, "digraph DFG {{").unwrap();
         writeln!(out, "    node [shape=box];").unwrap();
 
-        // Start/end marker nodes
-        writeln!(out, "    start [shape=circle, label=\"\", style=filled, fillcolor=black, width=0.3];").unwrap();
-        writeln!(out, "    end_node [shape=circle, label=\"\", style=filled, fillcolor=black, width=0.3];").unwrap();
-
         // Activity nodes with frequency as label
         for (activity, freq) in &self.activities {
             writeln!(out, "    \"{}\" [label=\"{}({})\"];", activity, activity, freq).unwrap();
-        }
-
-        // Start edges
-        for activity in &self.start_activities {
-            writeln!(out, "    start -> \"{}\";", activity).unwrap();
-        }
-
-        // End edges
-        for activity in &self.end_activities {
-            writeln!(out, "    \"{}\" -> end_node;", activity).unwrap();
         }
 
         // Directly-follows edges with frequency labels
